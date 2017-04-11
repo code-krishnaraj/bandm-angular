8c797ac15afc21a411440874d7fb0390779095c4
krishnaraj <krishnaraj@qburst.com>
2017-04-11 17:38:09 +0530

    Created home page in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent }  from './app.component';
+import { HomeComponent }   from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ 
+  	BrowserModule,
+  	AppRoutingModule
+  ],
+  declarations: [ 
+  	AppComponent,
+  	HomeComponent
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
